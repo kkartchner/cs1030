fcd337bde4c1e5676096a2dbafcbe96920dbf3c5
Ky Kartchner <kartchnerky@gmail.com>
2020-03-31 18:02:05 -0600

    Program 1 completed

---
+++ KKartchner_P1.java
@@ -0,0 +1,17 @@
+public class KKartchner_P1 
+{
+   public static void main (String[] args) {
+      double foodSalesTaxRate = .03; // Tax rate for food items
+      double nonFoodSalesTaxRate = .0675; // Tax rate for non-food items
+      
+      double foodBill = 150.0; // Total food bill
+      double nonFoodBill = 50.0; // Total non-food bill
+      
+      /* Total taxes = food taxes + nonfood taxes */
+      double totalTax = foodSalesTaxRate * foodBill 
+         + nonFoodSalesTaxRate * nonFoodBill; 
+      
+      /* Print statement with the total calculated taxes */
+      System.out.println("The total tax for your bill is: " + totalTax);
+   }
+}
